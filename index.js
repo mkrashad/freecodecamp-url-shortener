0f2c7aa661e415d711f7c1dd11f356e2b023c80f
Rashad Mahmudov <mkrashad25@gmail.com>
2023-09-01 20:51:58 +0200

    Add env vars and establish connection with db

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 require('dotenv').config();
+require('./db/db');
 const express = require('express');
 const cors = require('cors');
 const app = express();
@@ -10,15 +11,15 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
