0c6bfc50b3884a085718f4857eae7ffbfdf8f737
Rashad Mahmudov <mkrashad25@gmail.com>
2023-09-07 18:24:54 +0200

    Complete all points

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 require('./db/database');
 const express = require('express');
 const bodyparser = require('body-parser');
+const shortid = require('shortid');
+const dns = require('dns');
 const cors = require('cors');
 const urlService = require('./services/shortener.js');
 const app = express();
@@ -22,19 +24,35 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-// Your first API endpoint
-app.get('/api/hello', function (req, res) {
-  res.json({ greeting: 'hello API' });
-});
-
 app.post('/api/shorturl', function (req, res) {
-  const result = urlService.addUrl(req.body.url);
-  res.json({ original_url: req.body.url, short_url: 1 });
+  const url = req.body.url;
+  const httpRegex = /^(http|https)(:\/\/)/;
+  const divider = url.split('//');
+  if (!httpRegex.test(url)) {
+    return res.json({ error: 'invalid url' });
+  }
+  const subdomain = divider[1].split('www.');
+  dns.lookup(subdomain[1], async function (err, addresses, family) {
+    const id = shortid.generate();
+    await urlService.addUrl(url, id);
+    res.status(200).json({
+      original_url: url,
+      short_url: id,
+    });
+  });
 });
 
 app.get('/api/shorturl/:url', function (req, res) {
-  console.log(req.params.url);
-  res.redirect('https://www.google.com');
+  const input = req.params.url;
+  urlService.getURL(input).then((url) => {
+    const short_url = url[0]?.shortUrl;
+    const original_url = url[0]?.originalUrl;
+    if (short_url === input) {
+      res.status(200).redirect(`${original_url}`);
+    } else {
+      res.status(404).json({ error: 'Not found' });
+    }
+  });
 });
 
 app.listen(port, function () {
--- services/shortener.js
+++ services/shortener.js
@@ -1,19 +1,24 @@
 let URLModel = require('../model/urlSchema');
 
-const addUrl = (value) => {
+const addUrl = (url, id) => {
   const msg = new URLModel({
-    originalUrl: value,
-    shortUrl: 1,
+    originalUrl: url,
+    shortUrl: id,
   });
   msg
     .save()
-    .then((res) => {
+    .then((_) => {
       console.log('Data successfully added');
     })
     .catch((err) => {
       console.error(err);
     });
-  return msg
+  return msg;
 };
 
-module.exports = { addUrl };
+const getURL = (value) => {
+  const data = URLModel.find({ shortUrl: value }).exec();
+  return data;
+};
+
+module.exports = { addUrl, getURL };
