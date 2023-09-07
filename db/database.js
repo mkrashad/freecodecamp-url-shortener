0ab4aff2a55e3eaccc489d256009a3a8a49ceaea
Rashad Mahmudov <mkrashad25@gmail.com>
2023-09-07 20:29:24 +0200

    Finish the project

--- db/database.js
+++ db/database.js
@@ -9,7 +9,7 @@ class Database {
 
   _connect() {
     mongoose
-      .connect(`${process.env.MONGO_URI}/${process.env.DATABASE}`, {
+      .connect(process.env['MONGO_URI'], {
         useNewUrlParser: true,
       })
       .then(() => {
